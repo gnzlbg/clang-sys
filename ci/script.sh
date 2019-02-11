b3f63d2cec72298cfce4a491f9a08dc815101984
gnzlbg <gonzalobg88@gmail.com>
2019-02-11 22:14:39 +0100

    fixup

--- ci/script.sh
+++ ci/script.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
-    rvm get head || true
-fi
 
 set -ex
 
-RUST_BACKTRACE=1 cargo test -vv --features "$CLANG_VERSION assert-minimum" -- --nocapture
-if [ "${CLANG_VERSION}" \< "clang_3_7" ]; then
-    RUST_BACKTRACE=1 cargo test -vv --features "$CLANG_VERSION static" -- --nocapture
+FEATURES=
+if [ "$CLANG_VERSION" != "" ]; then
+    FEATURES="assert-minimum"
 fi
-RUST_BACKTRACE=1 cargo test -vv --features "$CLANG_VERSION assert-minimum runtime" -- --nocapture
+
+RUST_BACKTRACE=1 cargo test -vv --features "$CLANG_VERSION $FEATURES" -- --nocapture
+RUST_BACKTRACE=1 cargo test -vv --features "$CLANG_VERSION static $FEATURES" -- --nocapture
+RUST_BACKTRACE=1 cargo test -vv --features "$CLANG_VERSION runtime $FEATURES" -- --nocapture
 
 RUST_BACKTRACE=1 cargo test --manifest-path=clang-sys-test/Cargo.toml -vv -- --nocapture
